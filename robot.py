a6a6a8291bb2812b192a0dd2af63a926b807e16c
MatiasGuerra95 <matias.guerrac@gmail.com>
2025-01-03 11:19:55 -0300

    Edicion robot.py

--- robot.py
+++ robot.py
@@ -1,3 +1,4 @@
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,6 +9,13 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 import time
 import os
+
+# Configurar logging
+logging.basicConfig(
+    filename='script.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 # Cargar variables de entorno
 from dotenv import load_dotenv
@@ -35,88 +43,84 @@ def setup_driver():
 
 def login_sistema_requerimientos(driver):
     try:
-        print("Navegando al portal de sistema de requerimientos.")
+        logging.info("Navegando al portal de sistema de requerimientos.")
         driver.get("https://sistemaderequerimientos.cl/")
 
-        print("Intentando hacer clic en 'Soy Proveedor'...")
+        logging.info("Intentando hacer clic en 'Soy Proveedor'...")
         WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.ID, "tabs-icons-text-2-tab"))
         ).click()
-        print("Clic en 'Soy Proveedor' realizado.")
+        logging.info("Clic en 'Soy Proveedor' realizado.")
 
-        print("Ingresando credenciales...")
+        logging.info("Ingresando credenciales...")
         WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.ID, "inputUsername_recover"))
         ).send_keys(USER)
         driver.find_element(By.ID, "inputPassword_recover").send_keys(PASSWORD)
 
-        print("Intentando hacer clic en 'Iniciar Sesión'...")
+        logging.info("Intentando hacer clic en 'Iniciar Sesión'...")
         WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "div#tabs-icons-text-2 form button[type='submit']"))
         ).click()
-        print("Inicio de sesión realizado.")
+        logging.info("Inicio de sesión realizado.")
         time.sleep(5)
 
     except Exception as e:
-        print(f"Error durante el inicio de sesión: {e}")
+        logging.error(f"Error durante el inicio de sesión: {e}")
         raise
 
 def navegar_menu_soporte_operativo(driver):
     try:
-        print("Intentando hacer clic en 'Personal Externo'...")
-        personal_externo = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, "//a[@href='#module_hrm' and @role='button']"))
-        )
-        driver.execute_script("arguments[0].scrollIntoView(true);", personal_externo)
+        logging.info("Intentando hacer clic en 'Soporte operativo'...")
         WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[@href='#module_hrm' and @role='button']"))
-        ).click()
-        print("Clic en 'Personal Externo' realizado.")
+            EC.element_to_be_clickable((By.XPATH, "//a[@class='nav-link btn btn-default-a dropdown-toggle' and contains(text(),'Soporte operativo')]")
+        )).click()
+        logging.info("Clic en 'Soporte operativo' realizado.")
 
-        print("Intentando hacer clic en 'Estado de solicitudes Personal Externo'...")
-        estado_solicitudes = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, "//a[@href='/workflow/externalizacion-personal']"))
-        )
-        driver.execute_script("arguments[0].scrollIntoView(true);", estado_solicitudes)
+        logging.info("Intentando hacer clic en 'Personal Externo'...")
         WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[@href='/workflow/externalizacion-personal']"))
-        ).click()
-        print("Clic en 'Estado de solicitudes Personal Externo' realizado.")
+            EC.element_to_be_clickable((By.XPATH, "//a[@href='#module_hrm']//span[contains(text(),'Personal Externo')]")
+        )).click()
+        logging.info("Clic en 'Personal Externo' realizado.")
+
+        logging.info("Intentando hacer clic en 'Estado de solicitudes Personal Externo'...")
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//a[@href='/workflow/externalizacion-personal' and contains(text(),'Estado de solicitudes Personal Externo')]")
+        )).click()
+        logging.info("Clic en 'Estado de solicitudes Personal Externo' realizado.")
+
         time.sleep(2)
 
     except Exception as e:
-        print(f"Error navegando el menú: {e}")
-        driver.save_screenshot("error_navegacion_menu.png")
+        logging.error(f"Error navegando el menú: {e}")
         raise
-
-
 
 def ingresar_y_extraer_numero(driver):
     try:
-        print("Intentando hacer clic en el botón 'Ver' de la primera solicitud...")
+        logging.info("Intentando hacer clic en el botón 'Ver' de la primera solicitud...")
         boton_ver = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "a.btn.btn-sm.btn-primary"))
         )
         boton_ver.click()
-        print("Clic en el botón 'Ver' realizado.")
+        logging.info("Clic en el botón 'Ver' realizado.")
         time.sleep(3)
 
-        print("Intentando extraer el número de solicitud desde la página de detalles...")
+        logging.info("Intentando extraer el número de solicitud desde la página de detalles...")
         numero_solicitud = WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, "span.breadcrumb-item.active.current"))
         ).text
 
         numero_solicitud = numero_solicitud.replace("Solicitud N°", "").strip()
-        print(f"Número de solicitud extraído: {numero_solicitud}")
+        logging.info(f"Número de solicitud extraído: {numero_solicitud}")
         return numero_solicitud
 
     except Exception as e:
-        print(f"Error al ingresar o extraer el número de solicitud: {e}")
+        logging.error(f"Error al ingresar o extraer el número de solicitud: {e}")
         raise
 
 def actualizar_google_sheets(valor):
     try:
-        print("Intentando actualizar Google Sheets...")
+        logging.info("Intentando actualizar Google Sheets...")
         creds = Credentials.from_service_account_file("service_account.json", scopes=SCOPES)
         service = build("sheets", "v4", credentials=creds)
         values = [[valor]]
@@ -127,10 +131,10 @@ def actualizar_google_sheets(valor):
             valueInputOption="USER_ENTERED",
             body=body,
         ).execute()
-        print(f"{result.get('updates').get('updatedCells')} celda(s) actualizada(s) en Google Sheets.")
+        logging.info(f"{result.get('updates').get('updatedCells')} celda(s) actualizada(s) en Google Sheets.")
 
     except Exception as e:
-        print(f"Error actualizando Google Sheets: {e}")
+        logging.error(f"Error actualizando Google Sheets: {e}")
         raise
 
 def main():
@@ -145,4 +149,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
