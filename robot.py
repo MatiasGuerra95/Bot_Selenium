1c51b92564de1b28c48b9b756dbdffb1a459d66a
MatiasGuerra95 <matias.guerrac@gmail.com>
2025-01-08 16:12:09 -0300

    Llenar la tabla de solicitudes de requerimiento

--- robot.py
+++ robot.py
@@ -47,7 +47,7 @@ def setup_driver():
     options = Options()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--headless")  # Para ejecución en entornos sin GUI
+    options.add_argument("--headless")  # Para ejecución en entornos sin GUI
     options.add_argument("--window-size=1920,1080")
     service = Service("/usr/local/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=options)
